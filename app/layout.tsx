e835cc67bfca01f7857b7802888b5f32fe854712
Adarsh-Nagar-Ops <adarshnagar247@gmail.com>
2025-09-25 20:50:27 +0530

    Fixed SEO and Logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,13 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Adarsh Nagar's Portfolio",
   description: "This is Adarsh Nagar's Personal Portfolio website. I’m Adarsh Nagar, a second-year BSc Computer Science student at St. Xavier's College, Kolkata, with a passion for building web applications. I work with HTML, CSS, JavaScript, Typescript, React, Next.js,  PostgreSQL, Prisma, Node.js, Express.js, MongoDb and Websockets creating projects like a Real Time Chat Application, Personal Portfolio Website, Amazon clone, Netflix Clone, Course selling application Backend, a Paytm Cash App Clone to explore both frontend and backend development.I’m driven by the thrill of turning ideas into functional apps and constantly learning new ways to improve my craft. Excited to connect with fellow developers, share insights, and collaborate on creative tech projects. Let’s build something awesome together!",
-  keywords:["Adarsh Nagar","Portfolio","Adarsh Nagar Github","Adarsh Nagar St. Xavier's College Student","Adarsh Nagar Web Developer","Adarsh Nagar full stack developer"]
+  keywords:["Adarsh Nagar","Portfolio","Adarsh Nagar Github","Adarsh Nagar St. Xavier's College Student","Adarsh Nagar Web Developer","Adarsh Nagar full stack developer","Developer","Next.js","React","fullstack"],
+  openGraph:{
+    title: 'Adarsh Nagar - Portfolio',
+    description: 'Your professional portfolio',
+    type: 'website',
+  }
+
 };
 
 export default function RootLayout({
