fd49dedb0037da1239f2c291bb5d1386d6e48d6e
Adarsh-Nagar-Ops <adarshnagar247@gmail.com>
2025-09-25 20:20:18 +0530

    Removed Input

--- components/Input.tsx
+++
@@ -1,7 +0,0 @@
-export default function Input({label,placeholder}:any){
-
-    return <div className="flex flex-col items-self-start">
-        {label}
-        <input type="text" placeholder={placeholder}/>
-    </div>
-}
